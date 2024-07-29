1bde5fdb3060c89a3d323773383b514ffad0e390
ashishaman2020 <ashaman19262@gmail.com>
2024-07-29 16:13:04 +0530

    inheitance oops

--- OOP/Properties/inheritance/Main.java
+++ OOP/Properties/inheritance/Main.java
@@ -5,7 +5,13 @@ public class Main {
 //        Box box1 = new Box(10,20,30);
 //        System.out.println(box1.h + " " + box1.l + " " + box1.w);
 
-        BoxWeight box1 = new BoxWeight(10, 20, 30, 100);
-        System.out.println(box1.h);
+//        BoxWeight box1 = new BoxWeight(10, 20, 30, 100);
+//        System.out.println(box1.h);
+
+//        Box box5 = new BoxWeight(2,3,4,8);
+//        System.out.println(box5.w);
+
+        Box b1 = new Box();
+        System.out.println(b1.h + " " + b1.w);
     }
 }
